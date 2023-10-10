da56a33204afe73bd299f843127febe6bc88624a
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-10 20:04:44 +0100

    added component in canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-
+import { Canvas } from '@react-three/fiber'
+import { Suspense } from 'react'
+import Three from './component/Three'
 
 function App() {
  
   return (
     <>
-      
+      <Canvas>
+        <Suspense fallback={null}>
+          <Three />
+        </Suspense>
+      </Canvas>
     </>
   )
 }
