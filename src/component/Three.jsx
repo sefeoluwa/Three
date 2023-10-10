9790391ed5095db53b1f1d8fe22daf2b02f75545
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-10 21:05:31 +0100

    ball mesh, plane and camera added

--- src/component/Three.jsx
+++ src/component/Three.jsx
@@ -1,14 +1,31 @@
+import { PerspectiveCamera } from "@react-three/drei"
+import { angleToRadians } from "./angle"
+
 /* eslint-disable react/no-unknown-property */
 const Three = () => {
+
   return (
     <>
+    {/* Important for creating dimensions of the 3D element. It's like the eyes of the user or what the user can see. */}
+      <PerspectiveCamera makeDefault position={[0, 1, 5]}/>
 
-    <mesh>
-      <sphereGeometry args={[1, 32, 32]}/>
-      <meshStandardMaterial color='#ffffff' />
+      {/* defines the shape of the 3d element */}    
+      {/* defines the surface of the element */}
+    <mesh position={[0, 0.5, 0]}>
+      <sphereGeometry args={[0.5, 32, 32]}/>  
+      <meshStandardMaterial color='#ffffff' />   
     </mesh>
 
-    <ambientLight args={[`#f2f2f2` , 1]} />
+
+{/* Defines the background of the 3D element */}
+{/* Defines the surface of the background of the 3D element  */}
+    <mesh rotation={[-(angleToRadians(90)), 0, 0]}>
+      <planeGeometry args={[7, 7]} />
+      <meshStandardMaterial color='#1ea3dB'/>
+    </mesh>
+
+    {/* Sets lighting on the element */}
+    <ambientLight args={["#ffffff" , 1]} />
     </>
   )
 }
